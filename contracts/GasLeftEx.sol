85fae22541ffad7bb308468964de61cf22926de5
Gray <eungu0920@inu.ac.kr>
2023-09-25 21:30:33 +0900

    update

---
+++ contracts/GasLeftEx.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GasLeftEx {
+  uint save;
+
+  function check() public returns(uint, uint) {
+    uint bef = gasleft();
+    save = bef;
+    uint aft = gasleft();
+
+    return (bef, aft);
+  }
+}
