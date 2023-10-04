1f5257c9fe01fafaafe571ede6b2c4a81fa6497f
Gray <eungu0920@inu.ac.kr>
2023-10-05 04:46:15 +0900

    upload

---
+++ contracts/1_3.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+type UFixed16x2 is uint16;
+
+using {
+    // fill in
+} for UFixed16x2 global;
+
+function sub(UFixed16x2 a, UFixed16x2 b) pure returns (UFixed16x2) {
+    // fill in
+}
+
+contract Math {
+    // fill in
+}
+
